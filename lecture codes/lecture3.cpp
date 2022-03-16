11450e3694ace3f504755ade392945919bd7d27e
NikolaArden <anthonyardenuk@gmail.com>
2022-03-16 09:45:01 +0000

    Regular

--- lecture codes/lecture3.cpp
+++ lecture codes/lecture3.cpp
@@ -117,5 +117,22 @@ int main() {
         */
 
 
+/*
+        
+    // create two empty strings
+    std::string first_name {};
+    std::string surname {};
+
+    // input for first_name
+    std::cout << "\nPlease enter your first name: ";
+    std::getline(std::cin,first_name);
     
+    std::cout << "\nPlease enter your surname: ";
+    std::getline(std::cin,surname);
+
+    // output tests
+    std::cout << "your first name is: " << first_name << std::endl;
+    std::cout << "your surname is: " << surname << std::endl;
+*/
+
 };
--- main.cpp
+++ main.cpp
@@ -1,22 +1,8 @@
 #include <iostream> // cout; cin
 #include <string>   // strings
-#include <string_view> // string_views
 
 int main() {
-    
-    // create two empty strings
-    std::string first_name {};
-    std::string surname {};
 
-    // input for first_name
-    std::cout << "\nPlease enter your first name: ";
-    std::getline(std::cin,first_name);
-    
-    std::cout << "\nPlease enter your surname: ";
-    std::getline(std::cin,surname);
 
-    // output tests
-    std::cout << "your first name is: " << first_name << std::endl;
-    std::cout << "your surname is: " << surname << std::endl;
     
 };
